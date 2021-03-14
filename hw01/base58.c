996902039096bcd9b4dd1c2abdf523cdfa42c128
xsliva <xsliva@fi.muni.cz>
2021-03-14 18:43:37 +0100

    Encode added

--- hw01/base58.c
+++ hw01/base58.c
@@ -5,15 +5,72 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+char ascii_to_base58(int ascii) {
+    if (ascii <= 8) return (ascii + '1');
+    if (ascii <= 16) return (ascii + 56);
+    if (ascii <= 21) return (ascii + 57);
+    if (ascii <= 32) return (ascii + 58);
+    if (ascii <= 43) return (ascii + 'A' - 1);
+    if (ascii > 43) return (ascii + 'A');
+}
+
 bool encode(void)
-{
-    // TODO: implement simplified Base58 encode
+{   
+    int counter = 4;
+    int32_t baseconv = 0;
+    int8_t entry;
+    char prnt1;
+    char prnt2;
+    char prnt3;
+    char prnt4;
+    char prnt5;
+    char prnt6;
+    while ((entry=getchar()) != EOF){
+        counter--;
+        baseconv <<= 8;
+        baseconv += entry;
+        if (counter == 0) {
+            counter = 4;
+
+            prnt1 = ascii_to_base58(baseconv % 58);
+            baseconv /= 58;
+            prnt2 = ascii_to_base58(baseconv % 58);
+            baseconv /= 58;
+            prnt3 = ascii_to_base58(baseconv % 58);
+            baseconv /= 58;
+            prnt4 = ascii_to_base58(baseconv % 58);
+            baseconv /= 58;
+            prnt5 = ascii_to_base58(baseconv % 58);
+            baseconv /= 58;
+            prnt6 = ascii_to_base58(baseconv % 58);
+
+            baseconv = 0;
+            printf("%c%c%c%c%c%c", prnt6, prnt5, prnt4, prnt3, prnt2, prnt1);
+        }
+    }
+    if (counter < 4) {
+        baseconv <<= counter*8;
+        prnt1 = ascii_to_base58(baseconv % 58);
+        baseconv /= 58;
+        prnt2 = ascii_to_base58(baseconv % 58);
+        baseconv /= 58;
+        prnt3 = ascii_to_base58(baseconv % 58);
+        baseconv /= 58;
+        prnt4 = ascii_to_base58(baseconv % 58);
+        baseconv /= 58;
+        prnt5 = ascii_to_base58(baseconv % 58);
+        baseconv /= 58;
+        prnt6 = ascii_to_base58(baseconv % 58);
+
+        printf("%c%c%c%c%c%c", prnt6, prnt5, prnt4, prnt3, prnt2, prnt1);
+    }
+    putchar('\n');
     return true;
 }
 
 bool decode(void)
 {
-    // TODO: implement simplified Base58 decode
+    
     return false;
 }
 
